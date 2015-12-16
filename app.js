1499d696b24e1a7a6b38a849c8aae80e4bfec029
Ali Rawashdeh <ali@cinnamon.local>
2015-12-16 19:15:52 +0000

    added toggling of menus

--- app.js
+++ app.js
@@ -2,27 +2,12 @@ var express = require('express'); // Express web server framework
 var request = require('request'); // "Request" library
 var querystring = require('querystring');
 var cookieParser = require('cookie-parser');
-
-var client_id = process.env.client_id || '03ffe0cac0a0401aa6673c3cf6d02ced';// Your client id
-var client_secret = process.env.client_secret || 'a57c43efb9644574a96d6623fb8bfbc2'; // Your client secret
-var redirect_uri = process.env.redirect_uri || 'http://localhost:8888/callback'; // Your redirect uri
-
+var toggle = 1;
 /**
  * Generates a random string containing numbers and letters
  * @param  {number} length The length of the string
  * @return {string} The generated string
  */
-var generateRandomString = function(length) {
-  var text = '';
-  var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-
-  for (var i = 0; i < length; i++) {
-    text += possible.charAt(Math.floor(Math.random() * possible.length));
-  }
-  return text;
-};
-
-var stateKey = 'wunderlist_auth_state';
 
 var app = express();
 
@@ -31,87 +16,18 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'))
    .use(cookieParser());
 
-app.get('/login', function(req, res) {
-
-  var state = generateRandomString(16);
-  res.cookie(stateKey, state);
-
-  res.redirect('https://www.wunderlist.com/oauth/authorize?' +
-    querystring.stringify({
-      client_id: client_id,
-      redirect_uri: redirect_uri,
-      state: state
-    }));
+app.get('/toggle', function(req, res) {
+ if(toggle)
+ {toggle = 0;}
+ else {
+ {toggle = 1;}
+ }
+ res.send(200)
 });
 
-app.get('/callback', function(req, res) {
-
-  // your application requests refresh and access tokens
-  // after checking the state parameter
-
-  var code = req.query.code || null;
-  var state = req.query.state || null;
-  var storedState = req.cookies ? req.cookies[stateKey] : null;
-
-  if (state === null || state !== storedState) {
-    res.redirect('/#' +
-      querystring.stringify({
-        error: 'state_mismatch'
-      }));
-  } else {
-    res.clearCookie(stateKey);
-    var authOptions = {
-      url: 'https://www.wunderlist.com/oauth/access_token',
-      form: {
-        code: code,
-        client_id: client_id,
-        client_secret: client_secret,
-        redirect_uri: redirect_uri,
-        grant_type: 'authorization_code'
-      },
-      headers: {
-        'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
-      },
-      json: true
-    };
-
-    request.post(authOptions, function(error, response, body) {
-      if (!error && response.statusCode === 200) {
-
-        var access_token = body.access_token,
-            refresh_token = body.refresh_token;
-
-        var options = {
-          url: 'https://a.wunderlist.com/v1/me',
-          headers: {
-          'X-Access-Token': + access_token,
-          'X-Client-ID': authOptions.form.client_id
-        },
-          json: true
-        };
-
-        // use the access token to access the Wunderlist Web API
-        request.get(options, function(error, response, body) {
-          console.log(body);
-        });
-
-        // we can also pass the token to the browser to make requests from there
-        res.redirect('/#' +
-          querystring.stringify({
-            access_token: access_token,
-            client_id: authOptions.form.client_id,
-            refresh_token: refresh_token
-          }));
-
-
-      } else {
-        res.redirect('/#' +
-          querystring.stringify({
-            error: 'invalid_token'
-          }));
-      }
-    });
-  }
+app.get('/gettoggle', function(req, res) {
+    res.type('text/plain');
+    res.send('' + toggle);
 });
 
 app.listen(app.get('port'), function() {
