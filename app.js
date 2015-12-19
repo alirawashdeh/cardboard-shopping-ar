dff67f49fb9b74a106404258c3bba5e862792d1f
Ali Rawashdeh <ali@cinnamon.local>
2015-12-19 09:40:52 +0000

    changed the toggling to a command-based system

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ var express = require('express'); // Express web server framework
 var request = require('request'); // "Request" library
 var querystring = require('querystring');
 var cookieParser = require('cookie-parser');
-var toggle = 1;
+var command = '';
+var dirty = true;
 /**
  * Generates a random string containing numbers and letters
  * @param  {number} length The length of the string
@@ -16,18 +17,24 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'))
    .use(cookieParser());
 
-app.get('/toggle', function(req, res) {
- if(toggle)
- {toggle = 0;}
- else {
- {toggle = 1;}
- }
- res.send(200)
+app.get('/setcommand', function(req, res) {
+
+  command = req.query.command;
+  dirty = false;
+
+  res.send('' + command);
+ //res.send(200)
 });
 
-app.get('/gettoggle', function(req, res) {
+app.get('/getcommand', function(req, res) {
+
+    if(dirty)
+    {
+      command = '';
+    }
     res.type('text/plain');
-    res.send('' + toggle);
+    res.send('' + command);
+    dirty = true;
 });
 
 app.listen(app.get('port'), function() {
